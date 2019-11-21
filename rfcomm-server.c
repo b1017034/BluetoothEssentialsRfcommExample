19bdbb27b304342023f590b8f819984335eb6c33
b1017034 <b1017034@gmail.com>
2019-11-21 17:10:37 +0000

    [feat] rfcomm-server.c

--- rfcomm-server.c
+++ rfcomm-server.c
@@ -3,21 +3,29 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <wiringPi.h>
 
-int main (int argc, char** argv)
+#define GPIO_PIN 21
+
+int main(int argc, char **argv)
 {
-    struct sockaddr_rc loc_addr = { 0 } , rem_addr = { 0 } ;
-    char buf [1024] = { 0 } ;
-    int s, client, bytes_read ;
-    unsigned int opt = sizeof (rem_addr);
+    //struct sockaddr_rc loc_addr = {00:1B:DC:04:AE:D7}, rem_addr = {44:d4:e0:c8:45:11};
+    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    char buf[1024] = { 0 };
+    int s, client, bytes_read, status;
+    socklen_t opt = sizeof(rem_addr);
+    int ch = 1 < argc ? atoi(argv[1]) : 1;
+    printf("channel [%d]\n",ch);
 
     // allocate socket
-    s = socket (AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    printf("socket [%d]\n",s);
 
-    // bind socket to port 1 of the first available bluetooth adapter
-    loc_addr.rc_family = AF_BLUETOOTH ;
-    loc_addr.rc_bdaddr = *BDADDR_ANY ;
-    loc_addr.rc_channel =1;
+    // bind socket to port 22 of the first available
+    // local bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t) ch;
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
     // put socket into listening mode
@@ -26,17 +34,32 @@ int main (int argc, char** argv)
     // accept one connection
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
 
-    ba2str(&rem_addr.rc_bdaddr, buf);
+    ba2str( &rem_addr.rc_bdaddr, buf );
     fprintf(stderr, "accepted connection from %s\n", buf);
-    memset ( buf ,0, sizeof ( buf));
+    memset(buf, 0, sizeof(buf));
 
-    // read data from the client
-    bytes_read = recv(client, buf, sizeof(buf), 0);
-    if (bytes_read > 0) {
-        printf("received [%s]\n", buf);
+    while(1){
+        if(wiringPiSetupGpio() == -1) break;
+        if( digitalRead(GPIO_PIN) == HIGH){
+            // read data from the client
+            //bytes_read = read(client, buf, sizeof(buf));
+            printf("%i\n", digitalRead(GPIO_PIN));
+            status = write(client, "Hello", 5);
+	    printf("send");
+            /*if( bytes_read > 0 ) {
+                printf("received [%s]\n", buf);
+                printf("%i\n", status);
+
+            }
+            if( status == 0 ) {
+                status = write(client, "Hello", 5);
+            }
+
+            if( status < 0 ) perror("uh oh");*/
+        }
     }
 
-    // close connection
+    //close connection
     close(client);
     close(s);
     return 0;
